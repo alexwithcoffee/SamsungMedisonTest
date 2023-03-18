b92aed259f7344fc8dd899fc7ba0fe20b4bc8bf7
YeongDae Kim <alexwithcoffee@gmail.com>
2023-03-18 15:35:40 +0900

    [김영대] 새로운 영수증 추가
    -------------------- NEW Rental Receipt --------------------
    Customer : 고객
    ------------------------------------------------------------
    CHAPTER           |TITLE                |PERIOD |PRICE
    ------------------------------------------------------------
    REGULAR           |일반 1              |2            |2
    REGULAR           |일반 2              |3            |3.5
    NEW RELEASE    |신작 1              |1            |3
    NEW RELEASE    |신작 2              |2            |6
    CHILDRENS        |어린이 1          |3            |1.5
    CHILDRENS        |어린이 2          |4            |3
    ------------------------------------------------------------
    Amount owed is 19
    You earned 7 frequent renter points

--- Customer.cs
+++ Customer.cs
@@ -5,85 +5,102 @@ using System.Text;
 
 namespace VideoRental
 {
-  class Customer
-  {
-    public Customer(string name)
+    class Customer
     {
-        customerName = name;
-    }
-
-    public void addRental(Rental arg) { customerRental.Add(arg); }
-
-    public Boolean removeRental(Rental arg)
-    {
-        foreach (Rental rental in customerRental)
+        public Customer(string name)
         {
-            if(rental.getMovie() == arg.getMovie())
-            {
-                customerRental.Remove(rental);
-                return true;
-            }
-        }
-        return false;
-    }
-    public string getName() { return customerName; }
-
-    public string statement()
-    {
-        double totalAmount = 0.0;
-        int frequentRenterPoints = 0;
-        StringBuilder result = new StringBuilder();
-
-        result.AppendLine("Rental Record for" + getName());
-
-
-        IEnumerator<Rental> enumerator = customerRental.GetEnumerator();
-
-        for (; enumerator.MoveNext();)
-        {
-            double thisAmount = 0.0;
-            Rental each = enumerator.Current;
-
-            switch (each.getMovie().getPriceCode())
-            {
-                case Movie.REGULAR:
-                    thisAmount += 2.0;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-                case Movie.EXAMPLE_GENRE:
-                    thisAmount += each.getDaysRented() * 2;
-                    break;
-            }
-
-            // Add frequent renter points
-            frequentRenterPoints++;
-
-            // Add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints++;
-
-            // Show figures for this rental
-            result.AppendLine("\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString());
-            totalAmount += thisAmount;
+            customerName = name;
         }
 
-        result.AppendLine("Amount owed is " + totalAmount);
-        result.AppendLine("You earned " + frequentRenterPoints + " frequent renter points");
+        public void addRental(Rental arg) { customerRental.Add(arg); }
 
-        return result.ToString();
+        public Boolean removeRental(Rental arg)
+        {
+            foreach (Rental rental in customerRental)
+            {
+                if (rental.getMovie() == arg.getMovie())
+                {
+                    customerRental.Remove(rental);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string getName() { return customerName; }
+
+        public string statement()
+        {
+            double totalAmount = 0.0;
+            int frequentRenterPoints = 0;
+            StringBuilder result = new StringBuilder();
+            StringBuilder newResult = new StringBuilder();
+
+            result.AppendLine("Rental Record for" + getName());
+            newResult.AppendLine("-------------------- NEW Rental Receipt --------------------");
+            newResult.AppendLine("Customer : " + getName());
+            newResult.AppendLine("------------------------------------------------------------");
+            newResult.AppendLine(String.Format("{0,-15}|{1,-20}\t|{2,-7}|{3}", "CHAPTER","TITLE","PERIOD","PRICE"));
+            newResult.AppendLine("------------------------------------------------------------");
+
+
+            IEnumerator<Rental> enumerator = customerRental.GetEnumerator();
+
+            for (; enumerator.MoveNext();)
+            {
+                double thisAmount = 0.0;
+                Rental each = enumerator.Current;
+
+                switch (each.getMovie().getPriceCode())
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2.0;
+                        if (each.getDaysRented() > 2)
+                            thisAmount += (each.getDaysRented() - 2) * 1.5;
+                        newResult.Append(String.Format("{0,-15}|", "REGULAR"));
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.getDaysRented() * 3;
+                        newResult.Append(String.Format("{0,-15}|", "NEW RELEASE"));
+                        break;
+
+                    case Movie.CHILDRENS:
+                        thisAmount += 1.5;
+                        if (each.getDaysRented() > 3)
+                            thisAmount += (each.getDaysRented() - 3) * 1.5;
+                        newResult.Append(String.Format("{0,-15}|", "CHILDRENS"));
+                        break;
+                    case Movie.EXAMPLE_GENRE:
+                        thisAmount += each.getDaysRented() * 2;
+                        newResult.Append(String.Format("{0,-15}|", "EXAMPLE GENRE"));
+                        break;
+                }
+
+                // Add frequent renter points
+                frequentRenterPoints++;
+
+                // Add bonus for a two day new release rental
+                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                        && each.getDaysRented() > 1) frequentRenterPoints++;
+
+                // Show figures for this rental
+                result.AppendLine("\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString());
+                totalAmount += thisAmount;
+                newResult.AppendLine(String.Format("{0,-20}\t|{1,-7}|{2}", each.getMovie().getTitle(), each.getDaysRented().ToString(), thisAmount.ToString()));
+            }
+
+            result.AppendLine("Amount owed is " + totalAmount);
+            result.AppendLine("You earned " + frequentRenterPoints + " frequent renter points");
+
+            newResult.AppendLine("------------------------------------------------------------");
+            newResult.AppendLine("Amount owed is " + totalAmount);
+            newResult.AppendLine("You earned " + frequentRenterPoints + " frequent renter points");
+
+            result.AppendLine(newResult.ToString());
+
+            return result.ToString();
+        }
+
+        private string customerName;
+        private List<Rental> customerRental = new List<Rental>();
     }
-
-    private string customerName;
-    private List<Rental> customerRental = new List<Rental>();
-  }
 }
